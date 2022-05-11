854afec6284db3896b9aeedea078d9ea7da2ae1c
Pavel Eshmanski <Eshmanski@gmail.com>
2022-05-11 23:36:49 +0300

    '-'

--- src/main/java/com/rest/vkr/controllers/MainController.java
+++ src/main/java/com/rest/vkr/controllers/MainController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("api")
 public class MainController {
